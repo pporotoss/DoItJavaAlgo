381192699d32e6a9763e11396726ca82244a131e
Kwon <pporotoss@gmail.com>
2018-07-11 23:58:40 +0900

    ch04 실습 4-2까지

--- src/chap04/IntStack.java
+++ src/chap04/IntStack.java
@@ -19,4 +19,68 @@ public class IntStack {
             this.max = 0;
         }
     }
+    
+    public int push(int x) throws OverflowIntStackException {
+        if(ptr >= max) {
+            throw new OverflowIntStackException();
+        }
+    
+        return stk[ptr++] = x;
+    }
+    
+    public int pop() throws EmptyIntStackExcetpion {
+        if(ptr <= 0) {
+            throw new EmptyIntStackExcetpion();
+        }
+        return stk[--ptr];
+    }
+    
+    public int peek() throws EmptyIntStackExcetpion {
+        if(ptr <= 0) {
+            throw new EmptyIntStackExcetpion();
+        }
+        return stk[ptr - 1];
+    }
+    
+    public int indexOf(int x) {
+        for (int i = ptr - 1; i >= 0; i--) {
+            if(stk[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public void clear() {
+        ptr = 0;
+    }
+    
+    public int capacity() {
+        return max;
+    }
+    
+    public int size() {
+        return ptr;
+    }
+    
+    public boolean isEmpty() {
+        return ptr <= 0;
+    }
+    
+    public boolean isFull() {
+        return ptr >= max;
+    }
+    
+    public void dump() {
+        if(isEmpty()) {
+            System.out.println("스택이 비어있습니다.");
+        }
+        else {
+            for (int i = 0; i < ptr; i++) {
+                System.out.print(stk[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
 }
