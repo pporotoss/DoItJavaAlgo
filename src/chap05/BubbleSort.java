8748a279a5b88695344a97c6aab83c49a7369068
Kwon <pporotoss@gmail.com>
2018-07-16 23:54:18 +0900

    ch06 Q1까지

---
+++ src/chap05/BubbleSort.java
@@ -0,0 +1,33 @@
+package chap05;
+
+public class BubbleSort {
+    
+    static void swap(int[] array, int idx1, int idx2) {
+        int tmp = array[idx1];
+        array[idx1] = array[idx2];
+        array[idx2] = tmp;
+    }
+    
+    static void bubbleSort(int[] array, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if(array[j - 1] > array[j]) {
+                    swap(array, j - 1, j);
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] array = new int[]{22, 5, 11, 32, 120, 68, 70};
+        System.out.println("버블 정렬(버전 1)");
+        
+        bubbleSort(array, array.length);
+    
+        System.out.println("오름차순 정렬 완료!!");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array[" + i + "] = " + array[i]);
+        }
+    }
+    
+}
---
+++ src/chap05/BubbleSortEx.java
@@ -0,0 +1,33 @@
+package chap05;
+
+public class BubbleSortEx {
+    
+    static void swap(int[] array, int idx1, int idx2) {
+        int tmp = array[idx1];
+        array[idx1] = array[idx2];
+        array[idx2] = tmp;
+    }
+    
+    static void bubbleSort(int[] array, int n) {
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if(array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] array = new int[]{22, 5, 11, 32, 120, 68, 70};
+        System.out.println("버블 정렬(버전 1)");
+        
+        bubbleSort(array, array.length);
+        
+        System.out.println("오름차순 정렬 완료!!");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array[" + i + "] = " + array[i]);
+        }
+    }
+
+}
